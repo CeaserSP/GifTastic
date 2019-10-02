f6e53b20dd312322ddf2680ece5492992d04d882
Sheldon Perkins <csperkins18@gmail.com>
2019-10-02 13:24:20 -0400

    psuedo code

---
+++ logic.js
@@ -0,0 +1,11 @@
+// Array topics
+
+// for loop to appends topics to html
+
+//  get gifs by topic
+
+// use loop to diplay ten gifs of the topic
+
+// start and stop animation on click
+
+// display rating under gif

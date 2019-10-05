228599f4644559c59c8fe0c887dfb623904bff8a
Sheldon Perkins <csperkins18@gmail.com>
2019-10-05 18:33:41 -0400

    Giphy api url search and apikey

--- logic.js
+++ logic.js
@@ -12,7 +12,7 @@ for (var i= 0; i< topics.length; i++){
   button.appendTo(".buttons");
 }
 //  get gifs by topic
-
+var queryUrl= "https://api.giphy.com/v1/gifs/search?api_key=WsZdtxY39VcDAOsbH47aMxYUfXPCISyT&q=" + button + "&limit=10&offset=0&rating=G&lang=en"
 // use loop to diplay ten gifs of the topic
 
 // start and stop animation on click
